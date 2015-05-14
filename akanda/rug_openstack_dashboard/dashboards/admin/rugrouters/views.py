3a52d7afb2cbfa6b064d7268cb0d475a1bcf3760
Dmytro Vasylchenko <dmytro@vasylchenko.com>
2015-05-14 14:27:02 +0300

    Initial to akanda-horizon repo
    Change-Id: I3ee98229d2feea20abeb0fa996a9108bfba294a8

---
+++ akanda/rug_openstack_dashboard/dashboards/admin/rugrouters/views.py
@@ -0,0 +1,60 @@
+from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
+from django.utils.translation import ugettext_lazy as _
+
+from horizon import tables
+from horizon import forms
+from horizon import exceptions
+
+from openstack_dashboard import api
+
+from rug_openstack_dashboard.dashboards.admin.rugrouters import tables as router_tables
+from rug_openstack_dashboard.dashboards.admin.rugrouters import forms as rugrouters_forms
+from rug_openstack_dashboard.api.rug import RugClient
+
+
+rc = RugClient()
+
+
+class IndexView(tables.DataTableView):
+    table_class = router_tables.RouterTable
+    template_name = 'admin/rugrouters/index.html'
+
+    def has_prev_data(self, table):
+        return getattr(self, "_prev", False)
+
+    def has_more_data(self, table):
+        return getattr(self, "_more", False)
+
+    def get_data(self):
+        try:
+            routers, self._more = rc.get_routers(self.request)
+            return routers
+        except Exception:
+            url = reverse('horizon:admin:rugrouters:index')
+            exceptions.handle(self.request, _('Unable to retrieve routers\' details.'), redirect=url)
+
+
+class PollView(forms.ModalFormView):
+    form_class = rugrouters_forms.PollForm
+    template_name = 'admin/rugrouters/poll.html'
+    success_url = reverse_lazy('horizon:admin:rugrouters:index')
+
+
+class RebuildView(forms.ModalFormView):
+    form_class = rugrouters_forms.RebuildForm
+    template_name = 'admin/rugrouters/rebuild.html'
+    success_url = reverse_lazy('horizon:admin:rugrouters:index')
+
+    def get_context_data(self, **kwargs):
+        self.router = api.neutron.router_get(self.request, self.kwargs['router_id'])
+        context = super(RebuildView, self).get_context_data(**kwargs)
+        context["router_id"] = self.kwargs['router_id']
+        context["router_name"] = self.router['name']
+        return context
+
+    def get_initial(self):
+        return {
+            'router_id': self.kwargs['router_id'],
+            'router_name': self.get_context_data()['router_name']
+        }
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='akanda-rug-horizon',
+    version='0.1',
+    packages=find_packages("akanda"),
+    package_dir = {'':'akanda'},
+    install_requires=[
+        'requests',
+    ],
+)
+
