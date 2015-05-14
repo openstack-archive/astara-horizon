01d0534df63840d00ec732b3818b91f1ac7ed97c
Adam Gandelman <adamg@ubuntu.com>
2015-05-14 15:03:13 -0700

    Update setuptools for pbr, move extensions to subdir
    This updates setup.cfg/.py for pbr, renames the project to akanda-horizon
    and moves dashboard extensions into a subdirectory.
    
    Change-Id: I5cb1dad2e7dd7f47d0bac2ac39bdf34348fa6240

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-#!/usr/bin/env python
-from setuptools import setup, find_packages
+# Copyright 2015 Akanda, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
-setup(
-    name='akanda-rug-horizon',
-    version='0.1',
-    packages=find_packages("akanda"),
-    package_dir={'': 'akanda'},
-    install_requires=[
-        'requests',
-    ],
-)
+
+import setuptools
+
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
